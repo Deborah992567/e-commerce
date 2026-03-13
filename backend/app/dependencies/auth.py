d0ec21c9bc68c8229f02be9ca29b5b34d6f32a0e
DeborahIwuh <Uchechiiwuh12@icloud.com>
2026-03-13 23:32:05 +0100

    fixing more erros

--- backend/app/dependencies/auth.py
+++ backend/app/dependencies/auth.py
@@ -1,7 +1,6 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import  JWTError
-import jwt
+from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
 from app.core.config import settings
