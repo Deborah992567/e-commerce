34e671baffff6934f4c73167571fd5fbda8c513b
DeborahIwuh <Uchechiiwuh12@icloud.com>
2026-03-13 22:42:03 +0100

    made some changes

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -5,10 +5,14 @@ class UserCreate(BaseModel):
     password: str
     full_name: str | None = None
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
 class UserOut(BaseModel):
     id: int
     email: EmailStr
     role: str
 
     class Config:
-        orm_mode = True
+        from_attributes = True
